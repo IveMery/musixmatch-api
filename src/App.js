f373b67dcb4fc8185fe49d20f40adf0740b9541a
IvetteMery <ivette.mery@gmail.com>
2021-12-15 18:12:42 -0300

    armado arquitectura context api

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+// mi api key 537f9fc0aa5d60fdeaffcf7270e8a557
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
